86087462c62ab794033e621382d24e00e24563bc
bcwVelos <MVoight@BLOODCENTER.WI>
2016-10-21 11:37:21 -0500

    removed typo

--- test/js/deployed/736_VALIDATION3_VWD_PPG_Study/1007_BSQ/BCW_BSQ_Scoring.js
+++ test/js/deployed/736_VALIDATION3_VWD_PPG_Study/1007_BSQ/BCW_BSQ_Scoring.js
@@ -208,7 +208,7 @@ if (epistaxisPrimary[0].checked) {
 		}
 	}
 
-	if (epistaxisSeekMedAttn[0].checked) {}
+	if (epistaxisSeekMedAttn[0].checked) {
 	for (var i=0; i<epistaxisTreatedHow.length; i++) {
 		// And stick the checked ones onto an array...
 		if (epistaxisTreatedHow[i].checked) {
@@ -217,8 +217,8 @@ if (epistaxisPrimary[0].checked) {
 			if (Number(answerValue[1]) > noseBleedScore)
 				noseBleedScore = Number(answerValue[1]);
 		}
+		}
 	}
-}
 } 
 // Nose Bleed (Epistaxis) score
 noseBleedTotalScore[0].value = noseBleedScore;
